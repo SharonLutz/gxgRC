b0772d983b7855740891e460eb7ad29fcd2b76de
Annie Thwing <athwing@yahoo.com>
2018-05-31 02:28:39 -0600

    Add files via upload

--- R/gxgRC.R
+++ R/gxgRC.R
@@ -14,14 +14,14 @@ gxgRC <- function(n=1000,betaB=0.1,beta0=0,beta1=0.1,beta2=0.1,betaI=c(0,0.1,0.2
   if(!(varY>0)){stop("Error: varY must be greater than 0")}
   
   # Create a total results matrix
-  mat_total <- matrix(0,nrow=length(betaI),ncol=4)
-  colnames(mat_total) <- c("NOintP","intP0","intP1","intP2")
+  mat_total <- matrix(0,nrow=length(betaI),ncol=2)
+  colnames(mat_total) <- c("gxgNoInt","gxgInt")
   
   for(i in 1:nSim){
     
     # Create a results matrix for each simulation
-    mat_results <- matrix(0,nrow=length(betaI),ncol=4)
-    colnames(mat_results) <- c("NOintP","intP0","intP1","intP2")
+    mat_results <- matrix(0,nrow=length(betaI),ncol=2)
+    colnames(mat_results) <- c("gxgNoInt","gxgInt")
     
     for(bI.ind in 1:length(betaI)){
       
@@ -52,30 +52,12 @@ gxgRC <- function(n=1000,betaB=0.1,beta0=0,beta1=0.1,beta2=0.1,betaI=c(0,0.1,0.2
       
       # fit1: No interaction
       fit1 <- lm(Y~X1+X2)
-      if(summary(fit1)$coefficients[2,4] < alpha_level){mat_results[bI.ind,"NOintP"] <- mat_results[bI.ind,"NOintP"] +1 }
+      if(summary(fit1)$coefficients[2,4] < alpha_level){mat_results[bI.ind,"gxgNoInt"] <- mat_results[bI.ind,"gxgNoInt"] +1 }
       
       # fit2: With interaction
       fit2 <- lm(Y~X1+X2+X1*X2)
-      # For X2=0, beta1
-      if(summary(fit2)$coefficients[2,4] < alpha_level/3){mat_results[bI.ind,"intP0"] <- mat_results[bI.ind,"intP0"] +1 }
+      if(anova(fit1,fit2)$P[2] < alpha_level){mat_results[bI.ind,"gxgInt"] <- mat_results[bI.ind,"gxgInt"] +1 }
       
-      # For X2=1, beta1+betaI
-      # var(ax+by)=a^2var(x) + b^2var(y) + 2abcov(x,y)
-      # var(beta1+betaI)=var(beta1) + var(betaI) + 2cov(beta1,betaI)
-      est1 <- summary(fit2)$coefficients[2,1] + summary(fit2)$coefficients[4,1]
-      var1 <- vcov(fit2)[2,2] + vcov(fit2)[4,4] + 2*vcov(fit2)[2,4]
-      t1 <- est1/sqrt(var1)
-      pval1 <- 2*pt(abs(t1), summary(fit2)$df[2], lower=FALSE)
-      if(pval1 < alpha_level/3){ mat_results[bI.ind,"intP1"] <- mat_results[bI.ind,"intP1"] +1 }
-      
-      # For X2=2, beta1+2*betaI
-      # var(ax+by)=a^2var(x) + b^2var(y) + 2abcov(x,y)
-      # var(beta1+2betaI)=var(beta1) + 4var(betaI) + 4cov(beta1,betaI)
-      est2 <- summary(fit2)$coefficients[2,1] + 2*summary(fit2)$coefficients[4,1]
-      var2 <- vcov(fit2)[2,2] + 4*vcov(fit2)[4,4] + 4*vcov(fit2)[2,4]
-      t2 <- est2/sqrt(var2)
-      pval2 <- 2*pt(abs(t2), summary(fit2)$df[2], lower=FALSE)
-      if(pval2 < alpha_level/3){ mat_results[bI.ind,"intP2"] <- mat_results[bI.ind,"intP2"] +1 }
       
     } # End of betaI loop
     
@@ -91,11 +73,9 @@ gxgRC <- function(n=1000,betaB=0.1,beta0=0,beta1=0.1,beta2=0.1,betaI=c(0,0.1,0.2
     # Put plot code here
     pdf(plot.name)
     plot(-1,-1, xlim=c(min(betaI),max(betaI)), ylim=c(0,1),xlab="betaI values",ylab="")
-    points(betaI,mat_total[,"NOintP"],type="b",lty=2,col=1,pch=1)
-    points(betaI,mat_total[,"intP0"],type="b",lty=3,col=2,pch=2)
-    points(betaI,mat_total[,"intP1"],type="b",lty=4,col=3,pch=3)
-    points(betaI,mat_total[,"intP2"],type="b",lty=5,col=4,pch=4)
-    legend("topleft",lty=c(2:5),col=c(1:4),pch=c(1:4),legend=c("NOintP","intP0","intP1","intP2"))
+    points(betaI,mat_total[,"gxgNoInt"],type="b",lty=2,col=1,pch=1)
+    points(betaI,mat_total[,"gxgInt"],type="b",lty=3,col=2,pch=2)
+    legend("topleft",lty=c(2:3),col=c(1:2),pch=c(1:2),legend=c("gxgNoInt","gxgInt"))
     dev.off()
   }
   
