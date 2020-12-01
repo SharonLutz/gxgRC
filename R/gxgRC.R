6ec4309ceb56931130505609ad2fc31ba3b4a309
Sharon M. Lutz <SharonLutz@users.noreply.github.com>
2020-11-30 21:05:04 -0500

    Update gxgRC.R

--- R/gxgRC.R
+++ R/gxgRC.R
@@ -1,7 +1,7 @@
 gxgRC <-
 function(n=1000,nSim=1000,MAF1=0.2,gamma0=0,gammaX1=0.2,
 beta0=0,betaX1=0.1,betaX2=0.1,betaI=seq(from=0.1,to=0.5,by=0.1),varY=1,
-alpha_level=0.05,plot.pdf=T,plot.name="gxgRC.pdf",SEED=1){
+alpha_level=0.00000005,plot.pdf=T,plot.name="gxgRC.pdf",SEED=1){
   
 ##############################################################
   set.seed(SEED)
@@ -20,10 +20,9 @@ alpha_level=0.05,plot.pdf=T,plot.name="gxgRC.pdf",SEED=1){
 ##############################################################
 # Create a total results matrix
 ##############################################################
- mat_results <- matrix(0,nrow=length(betaI),ncol=5)
-  colnames(mat_results) <- c("model0:X1","model1:X1","model2:X1&XI","model2:X1","model2:XI")
-     
- 
+ mat_results <- matrix(0,nrow=length(betaI),ncol=3)
+  colnames(mat_results) <- c("model0:X1","model1:X1","model2:X1X2")
+   
 ##############################################################
 #cycle through simulations
 ##############################################################
@@ -59,11 +58,10 @@ if(floor(ii/100)==ceiling(ii/100)){print(paste("simulation",ii,"of",nSim))}
 ##############################################################
 # Fit model 2: With interaction
 ##############################################################
-      fitR <- lm(Y~X2) #reduced model
+      #fitR <- lm(Y~X2) #reduced model
       model2 <- lm(Y~X1+X2+X1*X2)
-      if(anova(fitR,model2)$P[2] < alpha_level){mat_results[bi,"model2:X1&XI"] <- mat_results[bi,"model2:X1&XI"] +1 }
-      if(summary(model2)$coefficients[2,4] < alpha_level){mat_results[bi,"model2:X1"] <- mat_results[bi,"model2:X1"] +1 }
-      if(summary(model2)$coefficients[4,4] < alpha_level){mat_results[bi,"model2:XI"] <- mat_results[bi,"model2:XI"] +1 }
+      #if(anova(fitR,model2)$P[2] < alpha_level){mat_results[bi,"model2:X1&XI"] <- mat_results[bi,"model2:X1&XI"] +1 }
+      if(summary(model2)$coefficients[4,4] < alpha_level){mat_results[bi,"model2:X1X2"] <- mat_results[bi,"model2:X1X2"] +1 }
 ##############################################################
 # Fit model 0: Just X1
 ##############################################################
@@ -89,10 +87,8 @@ model0 <- lm(Y~X1)
     plot(-1,-1, xlim=c(min(betaI),max(betaI)), ylim=c(0,1),xlab="betaI values",ylab="")
     points(betaI,mat_results[,"model0:X1"],type="b",lty=2,col=1,pch=1)
     points(betaI,mat_results[,"model1:X1"],type="b",lty=3,col=2,pch=2)
-    points(betaI,mat_results[,"model2:X1&XI"],type="b",lty=4,col=3,pch=3)
-    points(betaI,mat_results[,"model2:X1"],type="b",lty=5,col=4,pch=4)
-    points(betaI,mat_results[,"model2:XI"],type="b",lty=6,col=5,pch=5)
-    legend("topleft",lty=c(2:6),col=c(1:5),pch=c(1:5),legend=c("model0:X1","model1:X1","model2:X1&XI","model2:X1","model2:XI"))
+    points(betaI,mat_results[,"model2:X1X2"],type="b",lty=4,col=3,pch=3)
+    legend("topleft",lty=c(2:4),col=c(1:3),pch=c(1:3),legend=c("model0:Y~X1","model1:Y~X1+X2","model2:Y~X1+X2+X1*X2"))
     dev.off()
   }
   
