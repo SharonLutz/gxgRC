46b1ca3b81bcf13d3332d2e091a38db3ea3a71e2
Sharon M. Lutz <SharonLutz@users.noreply.github.com>
2020-12-16 23:22:52 -0500

    Update gxgRC.R

--- R/gxgRC.R
+++ R/gxgRC.R
@@ -1,8 +1,10 @@
 gxgRC <-
-function(n=1000,nSim=1000,MAF1=0.2,gamma0=0,gammaX1=0.2,
-beta0=0,betaX1=0.1,betaX2=0.1,betaI=seq(from=0.1,to=0.5,by=0.1),varY=1,
+function(n=1000,nSim=5000,MAF1=0.5,gamma0=0,gammaX1=0.3,
+beta0=0,betaX1=0.3,betaX2=0.3,betaI=seq(from=0.3,to=1,by=0.05),varY=1,
 alpha_level=0.00000005,plot.pdf=T,plot.name="gxgRC.pdf",SEED=1){
   
+##############################################################
+#Set the seed
 ##############################################################
   set.seed(SEED)
  
@@ -20,8 +22,18 @@ alpha_level=0.00000005,plot.pdf=T,plot.name="gxgRC.pdf",SEED=1){
 ##############################################################
 # Create a total results matrix
 ##############################################################
- mat_results <- matrix(0,nrow=length(betaI),ncol=3)
-  colnames(mat_results) <- c("model0:X1","model1:X1","model2:X1X2")
+ mat_results <- matrix(0,nrow=length(betaI),ncol=5)
+  colnames(mat_results) <- c("scenario1","scenario2","scenario3","scenario4","scenario5")
+  
+  #M0: Y~X1 Ho: no association between X1 and Y
+  #M1: Y~X1+X2 Ho: no association between X1 and Y
+  #M2: Y~X1+X2+X1*X2 Ho: no association between X1*X2 and Y
+  
+  #scenario 1: reject M0, M1, M2
+  #scenario 2: reject M0, M1
+  #scenario 3: reject M0, M2
+  #scenario 4: reject M0
+  #scenario 5: fail to reject M0
    
 ##############################################################
 #cycle through simulations
@@ -37,7 +49,7 @@ if(floor(ii/100)==ceiling(ii/100)){print(paste("simulation",ii,"of",nSim))}
 ##############################################################
 #Generate the data, X1 SNP of interest, X2 SNP in LD, outcome Y
 ##############################################################     
-      # Generate  SNP of interest    
+      # Generate  binary SNP of interest    
       X1 <- rbinom(n,1,MAF1) 
       
       #generate SNP 2 from SNP 1
@@ -48,31 +60,46 @@ if(floor(ii/100)==ceiling(ii/100)){print(paste("simulation",ii,"of",nSim))}
       # Generate Y based on X1 and X2
       muY<-beta0+betaX1*X1+betaX2*X2+betaI[bi]*X1*X2
       Y <- rnorm(n,muY,sqrt(varY))
+
+##############################################################
+# Fit 3 models
+##############################################################
+	#M0: Y~X1 Ho: no association between X1 and Y
+	m0 <- summary(lm(Y~X1))$coef[2,4] 
+    
+    #M1: Y~X1+X2 Ho: no association between X1 and Y
+    m1 <- summary(lm(Y~X1+X2))$coef[2,4] 
+    
+    #M2: Y~X1+X2+X1*X2 Ho: no association between X1*X2 and Y
+    m2 <- summary(lm(Y~X1+X2+X1*X2))$coef[4,4]  
       
 ##############################################################
-# Fit Model 1: No interaction
+# Determine scenarios
 ##############################################################
-      model1 <- lm(Y~X1+X2) #summary(fit1F)$coefficients[2,4]
-      if(summary(model1)$coefficients[2,4] < alpha_level){mat_results[bi,"model1:X1"] <- mat_results[bi,"model1:X1"] +1 }
-      
-##############################################################
-# Fit model 2: With interaction
-##############################################################
-      #fitR <- lm(Y~X2) #reduced model
-      model2 <- lm(Y~X1+X2+X1*X2)
-      #if(anova(fitR,model2)$P[2] < alpha_level){mat_results[bi,"model2:X1&XI"] <- mat_results[bi,"model2:X1&XI"] +1 }
-      if(summary(model2)$coefficients[4,4] < alpha_level){mat_results[bi,"model2:X1X2"] <- mat_results[bi,"model2:X1X2"] +1 }
-##############################################################
-# Fit model 0: Just X1
-##############################################################
-model0 <- lm(Y~X1) 
-      if(summary(model0)$coefficients[2,4] < alpha_level){mat_results[bi,"model0:X1"] <- mat_results[bi,"model0:X1"] +1 }
-      
+  #scenario 1: reject M0, M1, M2
+ if((m0<alpha_level)&(m1<alpha_level)&(m2<alpha_level)){
+ 	mat_results[bi,"scenario1"]<-mat_results[bi,"scenario1"]+1}
+   
+  #scenario 2: reject M0, M1
+ if((m0<alpha_level)&(m1<alpha_level)&(m2>alpha_level)){
+ 	mat_results[bi,"scenario2"]<-mat_results[bi,"scenario2"]+1}
+   
+  #scenario 3: reject M0, M2
+ if((m0<alpha_level)&(m1>alpha_level)&(m2<alpha_level)){
+ 	mat_results[bi,"scenario3"]<-mat_results[bi,"scenario3"]+1}
+   
+  #scenario 4: reject M0
+if((m0<alpha_level)&(m1>alpha_level)&(m2>alpha_level)){
+	mat_results[bi,"scenario4"]<-mat_results[bi,"scenario4"]+1}
+   
+  #scenario 5: fail to reject M0
+if((m0>alpha_level)){
+	mat_results[bi,"scenario5"]<-mat_results[bi,"scenario5"]+1}
+ 
 ##############################################################
 # end sims and save results
 ##############################################################
     } # End of betaI loop
-    
   } # End of nSim
   
   # Divide by the number of simulations 
@@ -84,15 +111,20 @@ model0 <- lm(Y~X1)
   if(plot.pdf){
     # Put plot code here
     pdf(plot.name)
-    plot(-1,-1, xlim=c(min(betaI),max(betaI)), ylim=c(0,1.1),xlab="betaI values",ylab="")
-    points(betaI,mat_results[,"model0:X1"],type="b",lty=2,col=1,pch=1)
-    points(betaI,mat_results[,"model1:X1"],type="b",lty=3,col=2,pch=2)
-    points(betaI,mat_results[,"model2:X1X2"],type="b",lty=4,col=3,pch=3)
-    legend("topleft",lty=c(2:4),col=c(1:3),pch=c(1:3),legend=c("model0:Y~X1","model1:Y~X1+X2","model2:Y~X1+X2+X1*X2"))
+    plot(-1,-1, xlim=c(min(betaI),max(betaI)), ylim=c(0,1.05),
+    xlab=expression(beta[I]),ylab="")
+    points(betaI,mat_results[,"scenario1"],type="b",lty=2,col=1,pch=1)
+    points(betaI,mat_results[,"scenario2"],type="b",lty=3,col=2,pch=2)
+    points(betaI,mat_results[,"scenario3"],type="b",lty=4,col=3,pch=3)
+    points(betaI,mat_results[,"scenario4"],type="b",lty=5,col=4,pch=4)
+    points(betaI,mat_results[,"scenario5"],type="b",lty=6,col=6,pch=5)
+
+legend("topleft",lty=c(2:6),col=c(1:4,6),pch=c(1:5),horiz=T,
+legend=c("S1","S2","S3","S4","S5"))
     dev.off()
   }
   
   # Write Table
-  list(mat_results)
-  
+  list(mat_results) 
 }
+##############################################################
